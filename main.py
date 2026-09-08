df4894a77307f0f2a5c287496e3f470169055449
kouljihate <kouljihate@users.noreply.github.com>
2026-09-08 17:27:49 +0200

    fix: expose app at module level for gunicorn

--- main.py
+++ main.py
@@ -64,9 +64,10 @@ def create_app():
         raise
 
 
+app = create_app()
+
 if __name__ == "__main__":
     try:
-        app = create_app()
         app.run(debug=True, host="0.0.0.0", port=5000)
     except Exception as e:
         app_logger.log_error(e, "__main__")
